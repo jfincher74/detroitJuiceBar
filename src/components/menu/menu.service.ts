03ddb6460dcc36e12f5112bce60b4c027beb40fb
Jay Fincher <fincher_jay@yahoo.com>
2019-05-21 16:54:54 -0400

    blah

--- src/components/menu/menu.service.ts
+++ src/components/menu/menu.service.ts
@@ -12,7 +12,7 @@ export class MenuService {
 
     getMenu(){
         return this._http
-        .get("../../../assets/drinks.json")
+        .get("/assets/drinks.json")
         .toPromise()
         .then(res => {
             this.menu = res.json()
